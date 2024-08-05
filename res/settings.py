1ae519005d3c9e9318b53bcab81dd369989ac8fc
olsen-adam <olsen.adam06@gmail.com>
2024-08-05 12:58:03 -0600

    "Updated settings.py: added json import, renamed __init__ to initSettings, modified default settings, and implemented json.dump to write settings to file."

--- res/settings.py
+++ res/settings.py
@@ -1,32 +1,34 @@
-import os
+import os, json
 
 globalSettings = {}
 
-def __init__():
+def initSettings():
     defaultSettings = {
         # UI Settings
         "theme": "light",
         "uiScaling": 1.0,
-        
+        "-":"-",
         # File Path Settings
-        "pathToPlayerList": "data/playerList.json",
-        "pathToSettingsList": "data/settings.json",
-        
+        "pathToPlayerList": "/data/playerList.json",
+        "--":"--",
         # Timer Settings
         "timerMinutes": 2,
         "timerSeconds": 30,
         "timerEnabled": True
     }
+    global globalSettings
     globalSettings = defaultSettings.copy()
 
 def writeFile():
     currDir = os.path.dirname(os.path.abspath(__file__))
     parentDir = os.path.abspath(os.path.join(currDir, os.pardir))
     
-    
-    # with open(, 'w') as f:
+    with open(parentDir + "/data/settings.json", 'w') as f:
+        json.dump(globalSettings,f,indent=2,ensure_ascii=False)
+        f.close()
 
 def main():
+    initSettings()
     writeFile()
     
 if __name__ == "__main__":
