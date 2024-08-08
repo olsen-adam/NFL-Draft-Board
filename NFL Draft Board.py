0174a16259c2671b162f5a44dda918f19324cd33
olsen-adam <olsen.adam06@gmail.com>
2024-08-07 18:10:59 -0600

    Core: Main program now creates an application, and can toggle between fullscreen mode

--- NFL Draft Board.py
+++ NFL Draft Board.py
@@ -1,4 +1,34 @@
 from res.nflAPI import refreshAPI
+import res.settings as settingsModule
+import customtkinter as ctk
 
-refreshAPI()
-input()
+class App(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.updateAPI()
+        
+        self.settings = settingsModule
+        # Set the appearance mode based on settings
+        ctk.set_appearance_mode(self.settings.loadSetting("theme"))
+        
+        
+        self.isFullScreen = self.settings.loadSetting("isFullscreen")
+        self.attributes("-fullscreen", self.isFullScreen)
+        
+        self.title("NFL Draft Board")
+        self.bind("<F11>", self.toggleFullScreen)
+
+    def updateAPI(self):
+        refreshAPI()
+        
+    def toggleFullScreen(self, *args):
+        self.isFullScreen = not self.isFullScreen
+        self.settings.setSetting("isFullscreen", self.isFullScreen)
+        self.attributes("-fullscreen", self.isFullScreen)
+
+def main():
+    app = App()
+    app.mainloop()
+    
+if __name__ == "__main__":
+    main()
